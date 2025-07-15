232f0b37dbc070930a187df9ef41d6cea533a7cd
Amar Kumar <amar2mail9@gmail.com>
2025-07-15 23:23:02 +0530

    Fixed samsung device responsible testimonial

--- src/components/Testimonial.jsx
+++ src/components/Testimonial.jsx
@@ -2,12 +2,12 @@ import React from "react";
 
 function Testimonial() {
   return (
-    <div className="md:px-[353px] px-[10px] pb-16 mt-16 md:flex-row flex-col flex items-center justify-center md:gap-16 gap-4">
+    <div className="md:px-[353px] px-[10px] pb-24 mt-12 lg:flex-row flex-col flex items-center justify-center lg:gap-16 gap-4">
       {/* image */}
       <div className="w-[120px] h-[120px] ">
         <img src="t.png" alt="" className="w-full h-full rounded-full" />
       </div>
-      <p className="md:w-[480px] w-[90%] h-[90px] text-center">
+      <p className="md:w-[480px] w-[90%] h-[90px] text-center ">
         There are many variations of passages of Lorem Ipsum available, but the
         majority have suffered alteration in some form, by injected humour, or
         randomised words which don't look even slightly believable.
